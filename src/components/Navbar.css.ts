292faa3b516ef24c6cd1afeb7f6248690f78c34e
otkrickey <rtk.otk@gmail.com>
2021-04-18 08:45:36 +0900

    Create React-CSS file for test

---
+++ src/components/Navbar.css.ts
@@ -0,0 +1,11 @@
+import { CSSProperties } from 'react';
+
+const navbar: CSSProperties = {
+    backgroundColor: '#060b26',
+    height: '80px',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+
+}
+export  { navbar }
