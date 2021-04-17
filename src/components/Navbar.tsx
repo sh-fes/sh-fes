2869c8f8403b9ef6e450dad4e3dd58cec50b8c98
otkrickey <rtk.otk@gmail.com>
2021-04-17 20:55:52 +0900

    Remove Unnecessary description

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { IconContext } from 'react-icons';
 import * as AiIcons from 'react-icons/ai';
 import * as FaIcons from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import './Navbar.scss'; // eslint-disable-line
+import './Navbar.scss';
 import { SidebarData } from './SidebarData';
 
 function Navbar() {
