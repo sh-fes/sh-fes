c3f8a7a68ca4e3a16c9187fed6a652256a7f6442
otkrickey <rtk.otk@gmail.com>
2021-04-18 08:45:01 +0900

    Change Functional-Component to Class-Component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { IconContext } from 'react-icons';
 import * as AiIcons from 'react-icons/ai';
 import * as FaIcons from 'react-icons/fa';
@@ -6,39 +6,55 @@ import { Link } from 'react-router-dom';
 import './Navbar.scss';
 import { SidebarData } from './SidebarData';
 
-function Navbar() {
-    const [sidebar, setSidebar] = useState(false);
+interface Props {}
 
-    const showSidebar = () => setSidebar(!sidebar);
+interface State {
+    sidebar: boolean;
+}
 
-    return (
-        <IconContext.Provider value={{ color: '#fff' }}>
-            <div className='navbar'>
-                <Link to='#' className='menu-bars'>
-                    <FaIcons.FaBars onClick={showSidebar} />
-                </Link>
-            </div>
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className='nav-menu-items' onClick={showSidebar}>
-                    <li className='navbar-toggle'>
-                        <Link to='#' className='menu-bars'>
-                            <AiIcons.AiOutlineClose />
-                        </Link>
-                    </li>
-                    {SidebarData.map((item, index) => {
-                        return (
-                            <li key={index} className={item.cName}>
-                                <Link to={item.path}>
-                                    {item.icon}
-                                    <span>{item.title}</span>
-                                </Link>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </nav>
-        </IconContext.Provider>
-    );
+class Navbar extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = { sidebar: false };
+        this.showSidebar = this.showSidebar.bind(this);
+    }
+    showSidebar() {
+        this.setState((state, props) => ({ sidebar: !state.sidebar }));
+    }
+    render() {
+        return (
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <div className='navbar'>
+                    <Link to='#' className='menu-bars'>
+                        <FaIcons.FaBars onClick={this.showSidebar} />
+                    </Link>
+                </div>
+                <nav
+                    className={
+                        this.state.sidebar ? 'nav-menu active' : 'nav-menu'
+                    }
+                >
+                    <ul className='nav-menu-items' onClick={this.showSidebar}>
+                        <li className='navbar-toggle'>
+                            <Link to='#' className='menu-bars'>
+                                <AiIcons.AiOutlineClose />
+                            </Link>
+                        </li>
+                        {SidebarData.map((item, index) => {
+                            return (
+                                <li key={index} className={item.cName}>
+                                    <Link to={item.path}>
+                                        {item.icon}
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </nav>
+            </IconContext.Provider>
+        );
+    }
 }
 
 export default Navbar;
