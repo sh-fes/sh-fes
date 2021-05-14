42b5995c9b77622f67a064bd9eaf4d69cd302fa3
otkrickey <rtk.otk@gmail.com>
2021-05-15 01:10:05 +0900

    Add:SampleCard カードデザインを試験的に追加。

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
+import { Card } from '../Components';
 import Base from './PageBase';
 
 interface Props {}
@@ -15,6 +16,14 @@ class Products extends Component<Props, States> {
                 <Base>
                     <div className='products'>
                         <h1>Products</h1>
+                        <ul>
+                            <li>asdasdasd</li>
+                            <li>asdasdasd</li>
+                            <li>asdasdasd</li>
+                            <li>asdasdasd</li>
+                            <li>asdasdasd</li>
+                        </ul>
+                        <Card />
                         <div style={{ height: '200vh' }}></div>
                     </div>
                 </Base>
