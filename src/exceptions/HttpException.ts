a4ea732a979a5e4ae50b7328cf9daaca8a215617
otkrickey <rtk.otk@gmail.com>
2021-07-26 01:37:24 +0900

    Express用のコードを削除

--- src/exceptions/HttpException.ts
+++
@@ -1,9 +0,0 @@
-export class HttpException extends Error {
-    public status: number
-    public message: string
-    constructor(status: number, message: string) {
-        super(message);
-        this.status = status || 500;
-        this.message = message;
-    }
-}
