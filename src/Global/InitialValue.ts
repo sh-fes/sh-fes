459616eefce7e0b23f4814aba5f66bcdb73c6bd0
otkrickey <rtk.otk@gmail.com>
2021-08-12 17:05:50 +0900

    カラーテーマの初期値をブラウザの規定のカラーテーマモードに変更

--- src/Global/InitialValue.ts
+++ src/Global/InitialValue.ts
@@ -1,6 +1,8 @@
 import { GlobalState, PayloadThemeType } from "./types";
 
+const PrefersColorSchema = () => { return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? PayloadThemeType.DARK : PayloadThemeType.LIGHT }
+
 export const InitialValue: GlobalState = {
-    theme: PayloadThemeType.LIGHT,
+    theme: PrefersColorSchema(),
     scroll: { up: false, down: true, last: 0 }
 }
